44cca94a7af72c917e8e645f37b986165bd08a4e
GabrielHdzDaw <gabrielhernandezdaw@gmail.com>
2025-05-08 13:16:05 +0200

    wipe

--- main.js
+++
@@ -1,8 +0,0 @@
-function setVideo(src) {
-    const video = document.getElementById('video360');
-    video.setAttribute('src', src);
-    video.play();
-  }
-
-  // Cargar un video por defecto al iniciar
-  window.onload = () => setVideo('videos/video1.mp4');
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-import './App.css'
-import Pico360Gallery from './Pico360Gallery';
-
-function App() {
-  return <Pico360Gallery />;
-}
-
-export default App
